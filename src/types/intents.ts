3717c68e4f03d40537def4bb30c402b2972a397b
omnifaced <omnifaced@proton.me>
2026-04-19 04:46:40 +0300

    fix: intent bits

--- src/types/intents.ts
+++ src/types/intents.ts
@@ -12,11 +12,12 @@ export const Intent = {
   Archetypes: 1 << 9,
   SpaceUpdates: 1 << 10,
   Voice: 1 << 11,
+  Calls: 1 << 12,
   ControlInteractions: 1 << 13,
 
-  AllNonPrivileged: 12253,
-  AllPrivileged: 34,
-  All: 12287,
+  AllNonPrivileged: (1 << 0) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11) | (1 << 13),
+  AllPrivileged: (1 << 1) | (1 << 5) | (1 << 12),
+  All: (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11) | (1 << 12) | (1 << 13),
 } as const
 
 /** Union of all intent bitfield values */
